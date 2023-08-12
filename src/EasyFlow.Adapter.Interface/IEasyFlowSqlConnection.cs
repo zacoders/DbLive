d1bf78dbdf646b910b4d0307c8d401f229e4203c
Vladimir Zatuliveter <zatuliveter@gmail.com>
2023-08-12 11:58:03 +0600

    code docs

--- src/EasyFlow.Adapter.Interface/IEasyFlowSqlConnection.cs
+++ src/EasyFlow.Adapter.Interface/IEasyFlowSqlConnection.cs
@@ -2,7 +2,12 @@
 
 public interface IEasyFlowSqlConnection
 {
+	/// <exception cref="EasyFlowSqlException"/>
 	void BeginTransaction(TransactionIsolationLevel isolationLevel);
+
+	/// <exception cref="EasyFlowSqlException"/>
 	void CommitTransaction();
+
+	/// <exception cref="EasyFlowSqlException"/>
 	void ExecuteNonQuery(string sqlStatementt);
 }
