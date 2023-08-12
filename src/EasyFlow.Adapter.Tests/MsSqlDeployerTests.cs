42f7f80aec3a0d1c21d19857505b749217253a24
Vladimir Zatuliveter <zatuliveter@gmail.com>
2023-08-12 11:41:54 +0600

    complex test with transaction.

--- src/EasyFlow.Adapter.Tests/MsSqlDeployerTests.cs
+++ src/EasyFlow.Adapter.Tests/MsSqlDeployerTests.cs
@@ -42,4 +42,42 @@ public class MsSqlDeployerTests : IntegrationTestsBase
 
 		cnn.ExecuteNonQuery(sql);
 	}
+
+	[TestMethod]
+	public void Complex_WithTransaction()
+	{
+		var cnn = _factory.GetDeployer(DBEngine.MSSQL, _cnnString);
+		
+		cnn.BeginTransaction(TransactionIsolationLevel.ReadCommitted);
+
+		cnn.ExecuteNonQuery(@"
+			drop table if exists dbo.Test
+		");
+
+		cnn.ExecuteNonQuery(@"
+			create table dbo.Test (
+				Id int identity
+			  , Name nvarchar(128) not null
+			  , CreatedUtc datetime2(0) not null
+					constraint DEF_Test_CreatedUtc default(sysutcdatetime())
+			)
+		");
+
+		cnn.ExecuteNonQuery(@"
+			insert into dbo.Test ( Name )
+			values ( 'Test1' ), ( 'Test2')
+		");
+
+
+		cnn.ExecuteNonQuery(@"
+			select *
+			from dbo.Test
+		");
+
+		cnn.ExecuteNonQuery(@"
+			drop table if exists dbo.Test
+		");
+
+		cnn.CommitTransaction();
+	}
 }
--- src/EasyFlow.Adapter.Tests/PgSqlDeployerTests.cs
+++ src/EasyFlow.Adapter.Tests/PgSqlDeployerTests.cs
@@ -42,4 +42,50 @@ public class DeployerTests : IntegrationTestsBase
 
 		cnn.ExecuteNonQuery(sql);
 	}
+
+
+	[TestMethod]
+	public void Complex_WithTransaction()
+	{
+		var cnn = _factory.GetDeployer(DBEngine.PostgreSql, _cnnString);
+
+		cnn.BeginTransaction(TransactionIsolationLevel.ReadCommitted);
+
+		cnn.ExecuteNonQuery(@"
+			drop sequence if exists public.s_test_id;
+			drop table if exists Test;
+		");
+
+		cnn.ExecuteNonQuery(@"
+			create sequence if not exists public.s_test_id
+			  increment 1
+			  minvalue 1000
+			  maxvalue 2147483647
+			  start 1000
+			  cache 1;
+
+			create table Test (
+				id int not null default nextval('public.s_test_id'::regclass)
+			  , name varchar(128) not null
+			  , created_utc timestamp without time zone not null default timezone('utc', now())
+			);
+		");
+
+		cnn.ExecuteNonQuery(@"
+			insert into Test ( Name )
+			values ( 'Test1' ), ( 'Test2')
+		");
+
+
+		cnn.ExecuteNonQuery(@"
+			select *
+			from Test
+		");
+
+		cnn.ExecuteNonQuery(@"
+			drop table if exists Test
+		");
+
+		cnn.CommitTransaction();
+	}
 }
