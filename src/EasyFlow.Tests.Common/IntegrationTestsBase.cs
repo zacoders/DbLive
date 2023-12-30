28a0a5ead9e5e51c3ff07d9045dd1a14b1201683
Vladimir Zatuliveter <zatuliveter@gmail.com>
2023-12-30 19:18:35 +0600

    EasyFlowTesting class improvements.  switch back to non static IServiceCollection Container in IntegrationTestsBase. #11

--- src/EasyFlow.Tests.Common/IntegrationTestsBase.cs
+++ src/EasyFlow.Tests.Common/IntegrationTestsBase.cs
@@ -2,29 +2,26 @@
 
 public abstract class IntegrationTestsBase : TestBase
 {
-	protected static readonly IServiceCollection Container;
-	
-	static IntegrationTestsBase()
+	protected readonly IServiceCollection Container;
+	private ServiceProvider? _serviceProvider;
+
+	protected IntegrationTestsBase(ITestOutputHelper output) : base(output)
 	{
 		Container = new ServiceCollection();
 		Container.AddSingleton<TestConfig>();
 	}
 
-	protected IntegrationTestsBase(ITestOutputHelper output) : base(output)
-	{		
+	/// <summary>
+	/// Returns ServiceProvider. Builds if needed or returns existing.
+	/// </summary>
+	/// <returns></returns>
+	protected IServiceProvider GetServiceProvider()
+	{
+		_serviceProvider ??= Container.BuildServiceProvider();
+		return _serviceProvider;
 	}
 
-	///// <summary>
-	///// Returns ServiceProvider. Builds if needed or returns existing.
-	///// </summary>
-	///// <returns></returns>
-	//protected IServiceProvider GetServiceProvider()
-	//{
-	//	serviceProvider ??= Container.BuildServiceProvider();
-	//	return serviceProvider;
-	//}
-
-	protected static TService GetService<TService>()
+	protected TService GetService<TService>()
 	{
 		return Container.BuildServiceProvider().GetService<TService>() ?? throw new Exception($"Cannot resolve {typeof(TService).Name}.");
 	}
--- src/EasyFlow/VSTests/EasyFlowPrepareTests.cs
+++ src/EasyFlow/VSTests/EasyFlowPrepareTests.cs
@@ -1,49 +1,21 @@
-﻿namespace EasyFlow.VSTests;
+﻿//namespace EasyFlow.VSTests;
 
-public class EasyFlowPrepareTests(
-	IEasyFlow _easyFlow, 
-	IEasyFlowProject _easyFlowProject, 
-	IUnitTestsRunner _unitTestsRunner
-) : IEasyFlowPrepareTests
-{
-	private static IServiceCollection Container = new ServiceCollection();
-	private string? _projectPath;
+//public class EasyFlowPrepareTests(
+//	IEasyFlow _easyFlow, 
+//	IEasyFlowProject _easyFlowProject, 
+//	IUnitTestsRunner _unitTestsRunner
+//) : IEasyFlowPrepareTests
+//{
+//	private static IServiceCollection Container = new ServiceCollection();
+//	private string? _projectPath;
 		
-	public TestItem[] TestItems { get; private set; } = [];
+//	public TestRunResult Run(TestItem testItem, string sqlConnectionString, EasyFlowSettings settings)
+//	{
+//		return _unitTestsRunner.RunTest(testItem, sqlConnectionString, settings);
+//	}
 
-	public TestRunResult Run(TestItem testItem, string sqlConnectionString, EasyFlowSettings settings)
-	{
-		return _unitTestsRunner.RunTest(testItem, sqlConnectionString, settings);
-	}
-
-	public void Load(string projectPath)
-	{
-		_projectPath = projectPath;
-		_easyFlowProject.Load(projectPath);
-		TestItems = [.. _easyFlowProject.GetTests()];
-	}
-
-	public void PrepareUnitTestingDatabase(string sqlConnectionString)
-	{
-		// TODO: drop database if exists and recreate it.
-
-		//if (!DbExists(SqlConnectionString))
-		//{			
-		//}
-
-		DeployParameters deployParams = new()
-		{
-			CreateDbIfNotExists = true,
-			DeployBreaking = true,
-			DeployCode = true,
-			DeployMigrations = true,
-			RunTests = false /* we will run tests in Visual Studio UI */
-		};
-
-		_easyFlow.DeployProject(
-			_projectPath ?? throw new Exception("Please call 'Load()' first."), 
-			sqlConnectionString, 
-			deployParams
-		);
-	}
-}
+//	public void PrepareUnitTestingDatabase(string sqlConnectionString)
+//	{
+		
+//	}
+//}
