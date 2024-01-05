7d31b7fd430ac2ca66b0d13df994aaa5ca4b24ae
Vladimir Zatuliveter <zatuliveter@gmail.com>
2024-01-05 15:00:50 +0600

    fixing issue with missed sql files in nuget. #11

--- demo/SqlServerConsole/UnitTest1.cs
+++ demo/SqlServerConsole/UnitTest1.cs
@@ -1,3 +1,7 @@
+using EasyFlow;
+using EasyFlow.Common;
+using EasyFlow.MSSQL;
+
 namespace SqlServerConsole;
 
 public class UnitTest1
@@ -5,6 +9,19 @@ public class UnitTest1
 	[Fact]
 	public void Test1()
 	{
+		string connectionString = "Server=.;Database=Test2;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=True;";
+		string projectPath = Path.GetFullPath(typeof(UnitTest1).Assembly.GetName().Name!);
+
+		var sqlDeploy = new EasyFlowBuilder()
+			.SqlServer()
+			.SetDbConnection(connectionString)
+			.SetProjectPath(projectPath)
+			.LogToConsole()
+			.CreateDeployer();
+
+		sqlDeploy.Deploy(parameters: DeployParameters.Default);
+
+		sqlDeploy.Deploy(parameters: DeployParameters.BreakingAndTests);
 
 	}
 }
