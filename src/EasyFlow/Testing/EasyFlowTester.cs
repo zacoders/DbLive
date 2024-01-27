76b29f233812cb9e75b685c72e0daad34d9ecee8
Vladimir Zatuliveter <zatuliveter@gmail.com>
2024-01-28 02:51:10 +0600

    test output formatting. #6

--- src/EasyFlow/Testing/EasyFlowTester.cs
+++ src/EasyFlow/Testing/EasyFlowTester.cs
@@ -15,22 +15,32 @@ public class EasyFlowTester(IEasyFlowProject _project, IUnitTestsRunner _unitTes
 	/// <param name="output"><see cref="ITestOutput"/></param>
 	/// <param name="relativePath">Relative path to the sql test.</param>
 	public TestRunResult RunTest(Action<string> writeLine, string relativePath)
-	{
-		writeLine($"Running unit test {relativePath}");
-		writeLine("");
-
+	{		
 		var testItem = TestsList[relativePath];
 
 		if (testItem.InitFileData is not null)
 		{
-			writeLine("Init content:");
+			string initFileTitle = $"== Init file: {testItem.InitFileData.RelativePath} ==";
+			writeLine(new string('-', initFileTitle.Length));
+			writeLine(initFileTitle);
+			writeLine(new string('-', initFileTitle.Length));
 			writeLine($"{testItem.InitFileData?.Content}");
+			writeLine("");
 		}
 
+		string testFileTitle = $"== Test file: {relativePath} ==";
+		writeLine(new string('-', testFileTitle.Length));
+		writeLine(testFileTitle);
+		writeLine(new string('-', testFileTitle.Length));
+
 		writeLine($"{testItem.FileData.Content}");
+		writeLine("");
 
 		var testRunResult = _unitTestsRunner.RunTest(testItem, new EasyFlowSettings());
 
+		writeLine("------------------");
+		writeLine("== Test output: ==");
+		writeLine("------------------");
 		writeLine(testRunResult.Output);
 
 		return testRunResult;
