c1d10ba097561b41f776037cfccdba7ddbecc286
Vladimir Zatuliveter <zatuliveter@gmail.com>
2024-01-28 00:03:44 +0600

    reorganization of tests #6

--- src/Demo/DemoMSSQL/Tests/Common/BeforeTests/prepare-data.sql
+++
@@ -1,5 +0,0 @@
-
-
-if not exists ( select * from dbo.Users where Name = 'Admin' )
-	throw 50001, 'Admin user must exists.', 0;
-
--- src/Demo/DemoMSSQL/Tests/Orders/AfterTests/cleanup-orders-data.sql
+++
@@ -1,5 +0,0 @@
-
-/*
-	I'm not sure if we need this at all. 
-	Tests executed in transaction with rollback. So there will be no data changes after the tests.
-*/
