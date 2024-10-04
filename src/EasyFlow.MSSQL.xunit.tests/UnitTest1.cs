fad22f469cad0e673cd822b9637909d7f17a58f4
zatuliveter <zatuliveter@gmail.com>
2024-10-04 14:36:10 +0600

    saving projectdir.user to the output directory together with sql files, so we can identify project path in VS. #38

--- src/EasyFlow.MSSQL.xunit.tests/UnitTest1.cs
+++ src/EasyFlow.MSSQL.xunit.tests/UnitTest1.cs
@@ -1,8 +1,16 @@
 
+using Xunit.Abstractions;
+
 namespace EasyFlow.MSSQL.xunit.tests;
 
-public class UnitTest1
+public class DBTests
 {
+	private readonly ITestOutputHelper _output;
+	public DBTests(ITestOutputHelper output)
+	{
+		_output = output;
+	}
+
 	[Fact]
 	public void Test1()
 	{
@@ -18,6 +26,8 @@ public class UnitTest1
 	[SqlFact(DisplayName = "SqlScript", SqlAssemblyName = "DemoMSSQL")]
 	public void Sql2(string sqlTestPath)
 	{
-
+		string projectNameVar = "DemoMSSQL_ProjectDir";
+		string? projectDir = Environment.GetEnvironmentVariable(projectNameVar);
+		_output.WriteLine($"{projectNameVar} = {projectDir}");
 	}
 }
