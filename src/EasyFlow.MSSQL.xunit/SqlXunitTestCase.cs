146ba51ce50ee2a6fb354d209f860acbf8093a85
Vladimir Zatuliveter <vzatuliveter@actsoft.com>
2025-02-23 02:37:34 +0600

    SqlXunitTestCase removed code that is not preventing from proper displaying of the test name and click on the test file in the VS test explorer.

--- src/EasyFlow.MSSQL.xunit/SqlXunitTestCase.cs
+++ src/EasyFlow.MSSQL.xunit/SqlXunitTestCase.cs
@@ -5,8 +5,7 @@ namespace EasyFlow.xunit;
 
 public class SqlXunitTestCase : XunitTestCase
 {
-	private string RelativeFilePath { get; set; }
-	private string TestFilePathVS { get; set; }
+	private string TestFileFullPath { get; set; }
 
 
 	[Obsolete("Called by the de-serializer", true)]
@@ -17,9 +16,9 @@ public class SqlXunitTestCase : XunitTestCase
 	/// </summary>
 	/// <param name="sink"></param>
 	/// <param name="testMethod"></param>
-	/// <param name="testFilePathVS">Test file path in Visual Studio IDE. It should point directly to the file in Visual Studio Project.</param>
-	/// <param name="relativeFilePath">Relative path to the testing file.</param>
-	public SqlXunitTestCase(IMessageSink sink, ITestMethod testMethod, string testFilePathVS, string relativeFilePath)
+	/// <param name="testFileFullPath">Test file path in Visual Studio IDE. It should point directly to the file in Visual Studio Project.</param>
+	/// <param name="testFileRelativePath">Relative path to the testing file.</param>
+	public SqlXunitTestCase(IMessageSink sink, ITestMethod testMethod, string testFileFullPath, string testFileRelativePath)
 		: base(
 			sink,
 			TestMethodDisplay.Method,
@@ -27,31 +26,14 @@ public class SqlXunitTestCase : XunitTestCase
 			testMethod
 		)
 	{		
-		TestFilePathVS = testFilePathVS;
-		RelativeFilePath = relativeFilePath;
+		TestFileFullPath = testFileFullPath;
 
-		TestMethodArguments = [relativeFilePath];
-	}
-
-	public override void Deserialize(IXunitSerializationInfo info)
-	{
-		base.Deserialize(info);
-		RelativeFilePath = info.GetValue<string>("MY_RelativeFilePath");
-		TestFilePathVS = info.GetValue<string>("MY_TestFilePathVS");
-	}
-
-	public override void Serialize(IXunitSerializationInfo info)
-	{
-		base.Serialize(info);
-		info.AddValue("MY_RelativeFilePath", RelativeFilePath, typeof(string));
-		info.AddValue("MY_TestFilePathVS", TestFilePathVS, typeof(string));
+		TestMethodArguments = [testFileRelativePath];
 	}
 
 	protected override string GetDisplayName(IAttributeInfo factAttribute, string displayName)
 	{
-		PopulateSourceInformation(TestFilePathVS, 1);
-
-		if (!string.IsNullOrWhiteSpace(RelativeFilePath)) return RelativeFilePath;
+		PopulateSourceInformation(TestFileFullPath, 1);
 
 		if (TestMethodArguments.Length > 0 && TestMethodArguments[0] is not null)
 		{
