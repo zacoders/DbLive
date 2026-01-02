4d29d7ea700689d89817cec8dbbfbe7099d24ce0
Vladimir Zatuliveter <vzatuliveter@actsoft.com>
2026-01-03 00:35:36 +0600

    DowngradeDeployer fixes, UNDO to a Specific Project Version Is Missing #67

--- src/DbLive/Deployers/Migrations/DowngradeDeployer.cs
+++ src/DbLive/Deployers/Migrations/DowngradeDeployer.cs
@@ -5,9 +5,14 @@ public class DowngradeDeployer(
 		ILogger _logger,
 		IDbLiveProject _project,
 		IDbLiveDA _da,
-		IMigrationItemDeployer _migrationItemDeployer
+		IMigrationItemDeployer _migrationItemDeployer,
+		ITransactionRunner _transactionRunner,
+		ISettingsAccessor projectSettingsAccessor
 	) : IDowngradeDeployer
 {
+
+	private readonly DbLiveSettings _projectSettings = projectSettingsAccessor.ProjectSettings;
+
 	public void Deploy(DeployParameters parameters)
 	{
 		// verify downgrade needed/allowed
@@ -38,6 +43,7 @@ public class DowngradeDeployer(
 		// get undo migrations
 		List<MigrationItemDto> undoMigrations = 
 			_da.GetMigrations()
+				.Where(m => m.ItemType == MigrationItemType.Undo)
 				.Where(m => m.Version > projectVersion)
 				.OrderByDescending(m => m.Version)
 				.ToList();
@@ -46,7 +52,7 @@ public class DowngradeDeployer(
 		// verify we have undo scripts for all migrations to be undone
 
 		HashSet<int> undoVersions = undoMigrations.Select(u => u.Version).ToHashSet();
-		HashSet<int> requiredVersions = Enumerable.Range(projectVersion + 1, projectVersion - databaseVersion).ToHashSet();
+		HashSet<int> requiredVersions = Enumerable.Range(projectVersion + 1, databaseVersion - projectVersion).ToHashSet();
 
 		List<int> missingUndoVersions = requiredVersions.Except(undoVersions).ToList();
 
@@ -59,28 +65,53 @@ public class DowngradeDeployer(
 			throw new DowngradeImpossibleException("Cannot perform downgrade due to missing undo scripts.");
 		}
 
+		Dictionary<int, string> undoContents = [];
+
 		// deploy undo migrations
 		foreach (MigrationItemDto undoDto in undoMigrations)
 		{
 			_logger.Information("Undoing migration version {version}", undoDto.Version);
-			
+
 			string? undoContent = _da.GetMigrationContent(undoDto.Version, MigrationItemType.Undo);
 
 			if (undoContent is null)
 			{
 				throw new DowngradeImpossibleException($"Undo content for migration version {undoDto.Version} is missing.");
 			}
-
-			MigrationItem undoItem = new()
-			{
-				MigrationItemType = MigrationItemType.Undo,
-				FileData = new FileData{ Content = undoContent, FilePath = "", RelativePath = ""},
-				Name = undoDto.Name				
-			};
-			
-			_migrationItemDeployer.Deploy(undoDto.Version, undoItem);
-
-			_logger.Information("Successfully undone migration version {version}", undoDto.Version);
+			undoContents[undoDto.Version] = undoContent;
 		}
+
+		_logger.Information(
+			"Downgrading database from version {databaseVersion} to {projectVersion}. Undo versions: {versions}",
+			databaseVersion,
+			projectVersion,
+			string.Join(", ", undoMigrations.Select(m => m.Version))
+		);
+
+		// all or nothing. online downgrades is not required.
+		_transactionRunner.ExecuteWithinTransaction(
+			true,
+			_projectSettings.TransactionIsolationLevel,
+			_projectSettings.DowngradeTimeout,
+			() => {
+				foreach (MigrationItemDto undoDto in undoMigrations)
+				{
+					MigrationItem undoItem = new()
+					{
+						MigrationItemType = MigrationItemType.Undo,
+						FileData = new FileData { 
+							Content = undoContents[undoDto.Version], 
+							FilePath = "", 
+							RelativePath = "" // todo: fill paths, required for logging.
+						},
+						Name = undoDto.Name
+					};
+
+					_migrationItemDeployer.Deploy(undoDto.Version, undoItem);
+
+					_logger.Information("Successfully undone migration version {version}", undoDto.Version);
+				}
+			}
+		);
 	}
 }
