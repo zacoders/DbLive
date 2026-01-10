51716b8fe114b11536e7262d13617cdaa7ee2de5
Vladimir Zatuliveter <vzatuliveter@actsoft.com>
2026-01-10 16:24:08 +0600

    fix: minor fixes, Convert DataAccess Method to Async #72

--- src/DbLive/Common/Transactions.cs
+++ src/DbLive/Common/Transactions.cs
@@ -2,19 +2,6 @@
 
 public class TransactionRunner : ITransactionRunner
 {
-	//public void ExecuteWithinTransaction(bool needTransaction, TranIsolationLevel isolationLevel, TimeSpan timeout, Action action)
-	//{
-	//	if (!needTransaction)
-	//	{
-	//		action();
-	//		return;
-	//	}
-
-	//	using TransactionScope _transactionScope = TransactionScopeManager.Create(isolationLevel, timeout);
-	//	action();
-	//	_transactionScope.Complete();
-	//}
-
 	public async Task ExecuteWithinTransactionAsync(
 		bool needTransaction,
 		TranIsolationLevel isolationLevel,
