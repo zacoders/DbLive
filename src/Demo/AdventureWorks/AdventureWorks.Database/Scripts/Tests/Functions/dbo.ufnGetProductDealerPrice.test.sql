dca0a6565353fbc0a83e23fb6c07358338897d34
Vladimir Zatuliveter <vzatuliveter@actsoft.com>
2026-02-12 11:11:06 +0600

    feat!: first full release.

--- src/Demo/AdventureWorks/AdventureWorks.Database/Scripts/Tests/Functions/dbo.ufnGetProductDealerPrice.test.sql
+++ src/Demo/AdventureWorks/AdventureWorks.Database/Scripts/Tests/Functions/dbo.ufnGetProductDealerPrice.test.sql
@@ -1,6 +1,10 @@
+-- Arrange
+declare @DealerDiscount decimal(5, 2) = 0.6 -- 60% hardcoded in the function
 
+-- Act
 select dbo.ufnGetProductDealerPrice(2, '2023-07-01') --as DealerPrice
 
+-- Assert
 select assert = 'rows-with-schema'
 
-select cast(925.00 * 0.6 /* 60% hardcoded @DealerDiscount*/ as money)
+select cast(925.00 * @DealerDiscount as money)
